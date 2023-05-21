c3c2373f0afac3143e3e2bc6d8e0b8a6e73615e5
Softwarl <134200542+Softwarl@users.noreply.github.com>
2023-05-21 11:59:36 +0200

    Update Locs.lua

--- Locs.lua
+++ Locs.lua
@@ -1,1 +1,23 @@
-
+local Locations = {
+    ["Jewelry"] = {
+        ["Position"] = Vector3.new(150, 18, 1373),
+    },
+        ["Bank"] = {
+        ["Position"] = Vector3.new(6.6, 21.74353, 773.439),
+        },
+        ["Museum"] = {
+        ["Position"] = Vector3.new(1093,139,1313),
+          },
+          ["CrimWarehouse"] = {
+        ["Position"] = Vector3.new(-268,18,1609),
+        },
+        ["CrimBaseVolc"] = {
+        ["Position"] = Vector3.new(2216.7,19,-2481.8),
+        },
+        ["GunShop"] = {
+        ["Position"] = Vector3.new(5.49,19,-1761),
+        },
+        ["Tomb"] = {
+        ["Position"] = Vector3.new(492.7,20.8,-491),
+    }
+}
